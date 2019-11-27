c62eeed2d5b67160efd7628fb9d75a477330274f
RadArcadeKid <jakegatsby935@gmail.com>
2019-11-26 20:16:24 -0700

    Fixed info.html, added more js functionality

--- views/info.js
+++ views/info.js
@@ -15,12 +15,7 @@ function load_boxes(num_sites)
 {
     delete_spinner(); //remove the spinner from the page
 
-    document.getElementById("header_info").innerHTML = "Your results from " + num_sites + " query(s)";
-    //tell the user how many sites' they've created
-
-
-    //NOTE - I'll need to update this in a more succinct way that isn't making a big-string and populating everything
-    //this is a nightmare and a half for creating new elements
+    populate_headers(num_sites); //Populate the header with number of sites
 
     var box_str = ''; //initialize empty box string to fill the div with
 
@@ -32,50 +27,134 @@ function load_boxes(num_sites)
                 '<nav>' +
                     '<ul>' +
                         '<li>' +
-                          '<button type="button" id="metadatabutton" class="btn btn-outline-info btn-lg"  data-toggle="button">Metadata</button>' +
+                          '<button type="button" id="metadatabutton_page_' + i + '" class="btn btn-outline-info btn-lg" data-toggle="collapse" data-target="#metadata_collapse_page_' + i + '" >Metadata</button>' +
                         '</li>' +
                         '<li>' +
-                          '<button type="button" id="textbutton" class="btn btn-outline-info btn-lg"  data-toggle="button">Text Analysis</button>' +
+                          '<button type="button" id="textbuttonpage_' + i + '"  class="btn btn-outline-info btn-lg" data-toggle="collapse" data-target="#text_collapse_page_' + i + '">Text Analysis</button>' +
                         '</li>' +
                     '</ul>' +
                 '</nav>' +
           '</div>' +
 
-
           '<div id="container_page_' + i + '" class="container" style="margin-top: -15px">' +
             '<article  id="article_page_' + i + '" >' +
-                  '<h3>' + 'https://nytimes.com (brief page title goes here)' + '</h3>' + //NOTE - this doesn't need an ID, and can be populated directly with the text from the API (specifically, the basic URL/title)
+                  '<h3>https://nytimes.com (brief page title goes here)</h3>' + //data[i] for url goes here
 
                   '<section class="fullwidth" style="align-content: center">' +
-                            'This is where a basic description about the website will go....' + //NOTE - this doesn't need an ID, and can be populated directly with the description text from the API
-                  '</section>' +
-                  '<section id="metadata_section_page_' + i + '"  class="halfwidth">' +
-                  //NOTE - these li elements shouldn't need unique ID's, and can be populated using the JSON from the API
-                    '<h5>Number of tags (metadata)</h5>' +
-                    '<ul id="ul_tag_list">' +
-                      '<li>div: 4</li>' +
-                      '<li>html: 7</li>' +
-                      '<li>ul: 9</li>' +
-                      '<li>li: 12</li>' +
-                      '<li>ol: 3</li>' +
-                      '<li>p: 77</li>' +
-                      '<li>header: 2</li>' +
-                      '<li>footer: 1</li>' +
-                      '<li>h1: 9</li>' +
-                      '<li>h2: 14</li>' +
-                    '</ul>' +
+                            'This is where a basic description about the website will go....' + //data[i] for description/Page title goes here
                   '</section>' +
 
-                  '<section id="piechart_section_page_' + i + '"  class="halfwidth">' +
-                    '<h5>Pie Chart Breakdown</h5>' +
-                        '[pi-chart goes here]' + //NOTE- this is where we'll need to put the PieChart from the JSON...note sure *how* or where we retreive it from (ask Andrew?)
-                  '</section>' +
-            '</article>' +
-          '</div>' +
-          '<br>'
-          ;
+                  '<div class="collapse" id="metadata_collapse_page_' + i + '">' +
+                        '<section id="metadata_section_page_' + i + '" class="halfwidth">' +
+                          '<h5><b>Number of tags (metadata)</b></h5>' +
+                          '<ul id="ul_tag_list">' +
+                            displayMetadataList() +
+                          '</ul>' +
+                        '</section>' +
+                        '<section id="piechart_section_page_' + i + '" class="halfwidth">' +
+                          '<h5><b>Pie Chart Breakdown</b></h5>' +
+                            displayMetadataPieChart() +
+                        '</section>' +
+                  '</div>' +
+
+                  '<div class = "collapse" id="text_collapse_page_' + i + '">' +
+                      '<section id="piechart_section_page_' + i + '" class="fullwidth" style="align-items: left;">' +
+                        '<h5><b>Text Analysis</b></h5>' +
+                          '<ul id="ul_tag_list">' +
+                            displayTextAnalysisList() +
+                          '</ul>' +
+                      '<section>' +
+                  '</div>' +
+                '</article>' +
+        '</div>' +
+        '<br>'
+        ;
       }
 
       $('#bigbox').html(box_str); //append to the string in the "bigbox", which is a big ol' div tag to store all this stuff
       //note - there's probably a *way* better way of doing this, but I'm not super familiar with javascript and editing HTML with it
   }
+
+//tells the user how many sites' they've queried
+//(shouldn't need updating)
+function populate_headers(num_sites){
+  if(num_sites == 1){
+    document.getElementById("header_info").innerHTML = "Here are your results from your query: ";
+  }
+  else{
+    document.getElementById("header_info").innerHTML = "Here are your results from all of your " + num_sites + " queries:";
+  }
+
+  document.getElementById("subheader_info").innerHTML = "<i>(To check out your data, click on the Metadata or Text Analysis buttons)</i>";
+}
+
+
+//displays an unordered list of metadata elements for a SINGLE page
+//gonna need to update this so that it'll populate with the API
+//
+//note - we should populate this ONLY with the data from 1 website, so that the data will get appended in the right order
+// maybe something like this:
+// function displayMetadataList(data[i]){
+function displayMetadataList(){
+  var metadata_str = '';
+
+  metadata_str = //API call for the given page goes here  (probably best to use a for loop)
+  '<li>div: 4</li>' +
+  '<li>html: 7</li>' +
+  '<li>ul: 9</li>' +
+  '<li>li: 12</li>' +
+  '<li>ol: 3</li>' +
+  '<li>p: 77</li>' +
+  '<li>header: 2</li>' +
+  '<li>footer: 1</li>' +
+  '<li>h1: 9</li>' +
+  '<li>h2: 14</li>'; //make sure to put data in between two <li></li> tags!
+
+  return metadata_str; //should always be returned as big string
+}
+
+//displays the pie chart of the metadta for the given element
+function displayMetadataPieChart(){
+    var html_pie_string = '';
+
+    html_pie_string = "[pie chart goes here]"; //populate the pie chart here, ideally as a string so we can return it
+    //i'm not sure what type of object the pie chart is, (image, css class? html?)
+    //but we can account for this later
+
+    return html_pie_string; //should always be returned as a string
+}
+
+//displays an unordered list of text elements for a SINGLE page
+//gonna need to update this so that it'll populate with the API
+function displayTextAnalysisList(){
+  var text_list_str = '';
+
+  text_list_str =
+  '<li>nouns: 44</li>' + //API call for the given page goes here
+  '<li>adjectives: 42</li>' +
+  '<li>names: 69</li>' +
+  '<li>whatever else: 420</li>'; //make sure to put data in between two <li></li> tags!
+
+  return text_list_str; //should always be returned as big string
+}
+
+
+
+
+///buttonState will allow the buttons to be toggled using the EventListener
+// don't touch!
+function buttonState(event){
+  //if it's actually a button, THEN we append active to it
+
+  //if it's ALREADY checked, then make it blue, otherwise, don't
+  if( event.target.classList.contains('btn')) {
+    if (event.target.classList.contains("active")) {
+      event.target.classList.remove("active");
+    }
+    else event.target.classList.add("active");
+  }
+
+}
+
+//add an event listenr to the document
+document.addEventListener("click", buttonState);
