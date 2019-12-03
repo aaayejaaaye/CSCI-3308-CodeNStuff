055e89c9cb613278df76b63e18dde94c12b1e74a
aaayejaaaye <ajones31256@gmail.com>
2019-12-02 18:24:46 -0700

    changed file structure

--- script.js
+++
@@ -1,7 +0,0 @@
-//add ids to input elements and add onclick="getWebURl()" on button
-
-function getWebURL(){
-document.getElementId('webpage_in_use').value = "url";
-var url = document.getElementId('webpage_in_use').value;
-alert(url);
-}
